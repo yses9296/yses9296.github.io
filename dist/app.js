379c349e7a2c2450e8d33d6c7ed2c84a3f5c4275
Eunseo Choi <yses9296@gmail.com>
2022-11-30 16:37:09 +0900

    init

---
+++ dist/app.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./client.jsx":
+/*!********************!*\
+  !*** ./client.jsx ***!
+  \********************/
+/***/ (function() {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: [BABEL] C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\client.jsx: React Refresh Babel transform should only be enabled in development environment. Instead, the environment is: \\\"production\\\". If you want to override this check, pass {skipEnvCheck: true} as plugin options. (While processing: \\\"C:\\\\\\\\Users\\\\\\\\yses9\\\\\\\\Desktop\\\\\\\\Portfolio\\\\\\\\portfolio_react_ver\\\\\\\\node_modules\\\\\\\\react-refresh\\\\\\\\babel.js\\\")\\n    at module.exports (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\react-refresh\\\\cjs\\\\react-refresh-babel.production.min.js:16:218)\\n    at async (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:38:33)\\n    at async (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:186:15)\\n    at C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:216:13\\n    at Generator.next (<anonymous>)\\n    at C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:184:21\\n    at Generator.next (<anonymous>)\\n    at Function.<anonymous> (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:21:3)\\n    at Generator.next (<anonymous>)\\n    at step (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:269:25)\\n    at evaluateAsync (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:291:5)\\n    at Function.errback (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:113:7)\\n    at errback (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:70:18)\\n    at async (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:188:17)\\n    at onFirstPause (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:216:13)\\n    at Generator.next (<anonymous>)\\n    at cachedFunction (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\caching.js:54:46)\\n    at cachedFunction.next (<anonymous>)\\n    at loadPluginDescriptor (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:264:42)\\n    at loadPluginDescriptor.next (<anonymous>)\\n    at loadPluginDescriptors (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:131:33)\\n    at loadPluginDescriptors.next (<anonymous>)\\n    at C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:159:21\\n    at Generator.next (<anonymous>)\\n    at loadFullConfig (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\full.js:143:5)\\n    at loadFullConfig.next (<anonymous>)\\n    at transform (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transform.js:20:45)\\n    at transform.next (<anonymous>)\\n    at step (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:269:25)\\n    at C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:223:11)\\n    at C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:189:28\\n    at C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:72:7\\n    at C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:113:33\\n    at step (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:287:14)\\n    at C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\yses9\\\\Desktop\\\\Portfolio\\\\portfolio_react_ver\\\\node_modules\\\\gensync\\\\index.js:223:11)\");\n\n//# sourceURL=webpack://portfolio_react_ver/./client.jsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./client.jsx"]();
+/******/ 	
+/******/ })()
+;
